4a1550bb4ee471bc0fc128d4914bfc51085a514f
jlovell <jason@zoomcc.com>
2013-08-06 19:23:48 -0500

    Initial rails commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SpecialMap::Application.config.secret_token = 'bf3989c101f0cd7d39091bd7ad065f1b151e0c9fee186dce9d916c325d0a5d690a5139a1905d58eac9facdaab31a8d5a696f267bceb09486328590eed4c8484f'
