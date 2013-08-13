57ce85070cdf18bb6ff9d752c919cdbc781917c9
jlovell <jason@zoomcc.com>
2013-08-12 21:38:15 -0500

    Add day indicator, conditional logic to show restaurants

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,2 +1,10 @@
 module HomeHelper
+
+	def has_specials?(place, day)
+		if @special = place.specials.find{ |x| x[:day] == day }
+			if !@special.food.empty? || !@special.drink.empty? || !@special.event.empty?
+				true
+			end
+		end
+	end
 end
